15d6be99558cac6148aed558d8409741a1b31c95
Przemek K Bitbucket <przemyslaw.klepacz.contact@gmail.com>
2020-03-26 23:29:07 +0100

    Hooks-> Part 8

--- burger-builder/src/components/UI/Modal/Modal.js
+++ burger-builder/src/components/UI/Modal/Modal.js
@@ -1,34 +1,28 @@
-import React, {Fragment, Component} from 'react';
+import React, {Fragment} from 'react';
 import PropTypes from "prop-types";
 
 import Backdrop from '../Backdrop/Backdrop';
 
-class Modal extends Component {
-
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        return (nextProps.show !== this.props.show);
-    }
-
-    render() {
-        return (
-            <Fragment>
-                <Backdrop show={this.props.show} click={this.props.clickBackdrop}/>
-                <div
-                    className="modal"
-                    style={{
-                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                        opacity: this.props.show ? '1' : '0'
-                    }}>
-                    {this.props.children}
-                </div>
-            </Fragment>
-        );
-    }
-}
+const Modal = props => {
+    return (
+        <Fragment>
+            <Backdrop show={props.show} click={props.clickBackdrop}/>
+            <div
+                className="modal"
+                style={{
+                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: props.show ? '1' : '0'
+                }}>
+                {props.children}
+            </div>
+        </Fragment>
+    );
+};
 
 Modal.propTypes = {
     show: PropTypes.bool.isRequired,
     clickBackdrop: PropTypes.func.isRequired
 };
 
-export default Modal;
+export default React.memo(Modal, (prevProps, nextProps) =>
+    nextProps.show === prevProps.show && nextProps.children === prevProps.children);
