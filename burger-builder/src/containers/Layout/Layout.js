b913360b63a6055f89f9a1c1dac9abf4b699706b
Przemek K Bitbucket <przemyslaw.klepacz.contact@gmail.com>
2020-03-26 22:25:37 +0100

    Hooks - Part 2

--- burger-builder/src/containers/Layout/Layout.js
+++ burger-builder/src/containers/Layout/Layout.js
@@ -1,40 +1,36 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {connect} from 'react-redux';
 
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
-class Layout extends Component {
-    state = {
-        showSideDrawer: false,
+const Layout = props => {
+    const [sideDrawerOpened, setSideDrawerOpened] = useState(false);
+
+    const sideDrawerClosedHandler = () => {
+        setSideDrawerOpened(false);
     };
 
-    sideDrawerClosedHandler = () => {
-        this.setState({showSideDrawer: false});
+    const sideDrawerOpenHandler = () => {
+        setSideDrawerOpened(true);
     };
 
-    sideDrawerOpenHandler = () => {
-        this.setState({showSideDrawer: true});
-    };
-
-    render () {
-        return (
-            <div className="layout">
-                <Toolbar
-                    isAuthenticated={this.props.isAuthenticated}
-                    showSideDrawer={this.sideDrawerOpenHandler}/>
-                <div className="layout__fill-toolbar"></div>
-                <SideDrawer
-                    isAuthenticated={this.props.isAuthenticated}
-                    isMenuOpen={this.state.showSideDrawer}
-                    closeSideDrawer={this.sideDrawerClosedHandler}/>
-                <main className="layout__main">
-                    {this.props.children}
-                </main>
-            </div>
-        );
-    }
-}
+    return (
+        <div className="layout">
+            <Toolbar
+                isAuthenticated={props.isAuthenticated}
+                showSideDrawer={sideDrawerOpenHandler}/>
+            <div className="layout__fill-toolbar"></div>
+            <SideDrawer
+                isAuthenticated={props.isAuthenticated}
+                isMenuOpen={sideDrawerOpened}
+                closeSideDrawer={sideDrawerClosedHandler}/>
+            <main className="layout__main">
+                {props.children}
+            </main>
+        </div>
+    );
+};
 
 const mapStateToProps = state => {
     return {
